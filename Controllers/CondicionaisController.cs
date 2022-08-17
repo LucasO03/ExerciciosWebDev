9d092616c430a7521793ae96addbea0600ab11ea
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2022-08-17 20:36:02 -0300

    Initial commit

---
+++ Controllers/CondicionaisController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exercicios.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercicios.Controllers
+{
+    public class CondicionaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CondicionalIf()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CondicionalIf(Partida p)
+        {
+            return View(p);
+        }
+
+        [HttpGet]
+        public IActionResult CondicionalSwitch()
+        {
+            return View();
+        }
+    }
+}
